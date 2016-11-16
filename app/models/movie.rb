953fc05dd9481998cfd6c2c10b87bd460d204367
“JulianBoralli” <jboralli@icloud.com>
2016-11-16 15:16:59 -0800

    Create models and migrations

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+
+	has_many :reviews
+
+	has_many :comments, as: :commentable
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+	has_many :reviews
+
+	has_many :comments
+end
