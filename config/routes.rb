eea48611311fb296f1bb60b71159e2ff961fb0b0
“JulianBoralli” <jboralli@icloud.com>
2016-11-16 16:38:22 -0800

    Create user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  # Session Routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # User Routes
+  get '/signup' => 'users#new'
+
+  resources :users, except: [:index, :new]
+  
 end
