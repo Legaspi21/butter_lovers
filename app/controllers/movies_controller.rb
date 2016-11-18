1261cb0b1d9de04264ee7e46df55fbdbe9304535
“JulianBoralli” <jboralli@icloud.com>
2016-11-17 16:39:06 -0800

    Create review form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,6 +23,7 @@ class MoviesController < ApplicationController
 
 	def show
 		@movie = Movie.find(params[:id])
+		@review = Review.new
 	end 
 
 end 
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
   belongs_to :movie
 
   has_many :comments, as: :commentable
+
+
+	validates_presence_of :body
+	validates_presence_of :rating
 end
