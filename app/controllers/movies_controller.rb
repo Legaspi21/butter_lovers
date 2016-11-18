04f69b9a158d760d665167c6e5d62dde195a39f8
“JulianBoralli” <jboralli@icloud.com>
2016-11-18 14:39:36 -0800

    Add ajax do review post

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,30 +1,30 @@
 class MoviesController < ApplicationController
-
+	before_action :current_user, only: [:show]
 
 	def index 
-		# Movie.where("category like ?", "%comedy%")
-	p params
 	@movie_title = params[:movie_title]
   if @movie_title
-      @movies = Movie.search(@movie_title)
-      render :index
+    @movies = Movie.search(@movie_title)
+    render :index
   else 
-      @movies = Movie.all
+    @movies = Movie.all
   end
 
 	@category = params[:category]
 
-		if @category
-			@movies = Movie.where("category like ?", "%#{@category}%")
-		else 
-			@movies = Movie.all
-		end 
+	if @category
+		@movies = Movie.where("category like ?", "%#{@category.capitalize}%")
+	else 
+		@movies = Movie.all
+	end 
 	end 
 
 	def show
 		@movie = Movie.find(params[:id])
 		@review = Review.new
 		@comment = Comment.new
+		@reviews = @movie.reviews
+
 	end 
 
 end 
