f4f97d0c424e7f282d05da636f210d7b0075992f
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-24 23:28:12 -0800

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,34 +1,30 @@
 class MoviesController < ApplicationController
-	before_action :current_user, only: [:show]
+  before_action :current_user, only: [:show]
 
 	def index 
-	@movie_title = params[:movie_title]
-  if @movie_title
-    @movies = Movie.search(@movie_title)
-    render :index
-  else 
-    @movies = Movie.all
-  end
-
-	@category = params[:category]
-
-	if @category
-		@movies = Movie.where("category like ?", "%#{@category.capitalize}%")
-	else 
-		@movies = Movie.all
-	end 
+	  @movie_title = params[:movie_title]
+    if @movie_title
+      @movies = Movie.search(@movie_title)
+      render :index
+    else 
+      @movies = Movie.all
+    end
+	  @category = params[:category]
+	  if @category
+		  @movies = Movie.where("category like ?", "%#{@category.capitalize}%")
+	  else 
+		  @movies = Movie.all
+	  end 
 	end 
 
 	def show
 		@movie = Movie.find(params[:id])
 		@review = Review.new
 		@comment = Comment.new
-		# binding.pry
 		# @reviews = @movie.reviews.sort {|a,b| (a.user.is_critic <=> b.user.is_critic}
 		@reviews = @movie.reviews.sort_by {|a| a.user.is_critic ? 0 : 1 }
-
 		@comments = @movie.comments
 	end 
-
 end 
 
+    
